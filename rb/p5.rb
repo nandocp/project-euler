49ec5372bfcbad23cf9a3499de005974896cae9f
Fernando Caçador <fcpontes@gmail.com>
2024-06-12 12:02:48 -0300

    Update p5.rb

--- rb/p5.rb
+++ rb/p5.rb
@@ -1,11 +1,13 @@
 # 2520  is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
-counter = 1
+counter = 0
 found = false
 
 while !found
-  break if (2..20).all? { |nr| counter % nr == 0 }
-  
   counter += 1
+  next unless counter.even?
+  break if (3..20).all? { |nr| counter % nr == 0 }
+  
+  puts "#{counter}: false"
 end
